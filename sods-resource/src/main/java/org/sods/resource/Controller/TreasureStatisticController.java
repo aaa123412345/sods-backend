ac698be6ee28d178507553c1166f758d1b1b2a43
kawaii-ng <nkwn721.dev@gmail.com>
2023-03-10 17:07:15 +0800

    Deleted AR Game Answer Table; Updated Treasure Services

--- sods-resource/src/main/java/org/sods/resource/Controller/TreasureStatisticController.java
+++ sods-resource/src/main/java/org/sods/resource/Controller/TreasureStatisticController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/ARGame/treasureStatistic")
+@RequestMapping("/ARGame/treasureStatistics")
 public class TreasureStatisticController {
 
     @Autowired
